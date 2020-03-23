2c8ea5714c8f76cd496a70293caf8ae4e488fdeb
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-03-23 11:43:14 +0100

    Add workflow-engine project

---
+++ engine/workflow-engine/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.70"
+}
+
+group = "com.zenaton.engine"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
