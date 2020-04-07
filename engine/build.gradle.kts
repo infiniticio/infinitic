a20c38a17cbf6b7bc962f65ee2a9dbd2923356d5
Gilles Barbier <gilles@zenaton.com>
2020-04-08 01:06:40 +0200

    add pulsar functions auto-update after build

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 plugins {
     kotlin("jvm") version "1.3.70"
@@ -15,7 +17,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-
     implementation("org.apache.pulsar:pulsar-client:2.5.+")
     implementation("org.apache.pulsar:pulsar-functions-api:2.5.+")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.10.+")
@@ -39,5 +40,32 @@ tasks {
 tasks {
     build {
         dependsOn(shadowJar)
+        finalizedBy(buildFinalizer)
     }
+}
+
+val buildFinalizer by tasks.registering {
+    doLast {
+        updatePulsarFunction("com.zenaton.engine.workflows.functions.State", "workflows")
+    }
+}
+
+fun updatePulsarFunction(className: String, topic: String) {
+    println("Updating $className in $topic")
+    val cmd = arrayOf("docker-compose", "exec", "-T", "pulsar", "bin/pulsar-admin", "functions", "update", "--jar", "/zenaton/engine/build/engine-1.0-SNAPSHOT-all.jar", "--classname", className, "--inputs", topic)
+    val p = Runtime.getRuntime().exec(cmd)
+    val output = getOutput(p)
+    val error = getError(p)
+    var line: String? = ""
+    while (output?.readLine().also { line = it } != null) println(line)
+    while (error?.readLine().also { line = it } != null) println(line)
+    p.waitFor()
+}
+
+fun getOutput(p: Process): BufferedReader? {
+    return BufferedReader(InputStreamReader(p.inputStream))
+}
+
+fun getError(p: Process): BufferedReader? {
+    return BufferedReader(InputStreamReader(p.errorStream))
 }
