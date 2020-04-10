f436da032ac36e708f325de95dc29327a0358d8b
Gilles Barbier <gilles@zenaton.com>
2020-04-10 02:12:05 +0200

    fix text visibility

--- engine/src/test/kotlin/com/zenaton/engine/workflows/state/StepTests.kt
+++ engine/src/test/kotlin/com/zenaton/engine/workflows/state/StepTests.kt
@@ -8,66 +8,64 @@ import io.kotest.matchers.shouldBe
 fun getStep() = Step.Id(UnitStepId())
 
 class StepTests : StringSpec({
-    "Step.Id by default is not completed " {
-        val step1 = getStep()
+    "Step is not completed by default" {
+        val step = getStep()
 
-        step1.isCompleted() shouldBe false
+        step.isCompleted() shouldBe false
     }
 
-    "Step.And will be completed only if all elements are completed" {
-        val step1 = getStep()
-        val step2 = getStep()
-        val step3 = getStep()
-        val step4 = Step.And(listOf(step1, step2, step3))
+    "Condition to complete (A AND B AND C)" {
+        val stepA = getStep()
+        val stepB = getStep()
+        val stepC = getStep()
+        val step = Step.And(listOf(stepA, stepB, stepC))
 
-        step4.isCompleted() shouldBe false
-        step1.completed = true
-        step4.isCompleted() shouldBe false
-        step2.completed = true
-        step4.isCompleted() shouldBe false
-        step3.completed = true
-        step4.isCompleted() shouldBe true
+        step.isCompleted() shouldBe false
+        stepA.completed = true
+        step.isCompleted() shouldBe false
+        stepB.completed = true
+        step.isCompleted() shouldBe false
+        stepC.completed = true
+        step.isCompleted() shouldBe true
     }
 
-    "Step.Or will be completed if at least 1 elements is completed" {
-        val step1 = getStep()
-        val step2 = getStep()
-        val step3 = getStep()
-        val step4 = Step.Or(listOf(step1, step2, step3))
+    "Condition to complete (A OR B OR C)" {
+        val stepA = getStep()
+        val stepB = getStep()
+        val stepC = getStep()
+        val step = Step.Or(listOf(stepA, stepB, stepC))
 
-        step4.isCompleted() shouldBe false
-        step1.completed = true
-        step4.isCompleted() shouldBe true
-        step2.completed = true
-        step4.isCompleted() shouldBe true
-        step3.completed = true
-        step4.isCompleted() shouldBe true
+        step.isCompleted() shouldBe false
+        stepA.completed = true
+        step.isCompleted() shouldBe true
+        stepB.completed = true
+        step.isCompleted() shouldBe true
+        stepC.completed = true
+        step.isCompleted() shouldBe true
     }
 
-    "Step.Or with nested step" {
-        val step1 = getStep()
-        val step2 = getStep()
-        val step3 = Step.Or(listOf(step1, step2))
-        val step4 = getStep()
-        val step5 = Step.Or(listOf(step3, step4))
+    "Condition to complete (A OR (B OR C))" {
+        val stepA = getStep()
+        val stepB = getStep()
+        val stepC = getStep()
+        val step = Step.Or(listOf(stepA, Step.Or(listOf(stepB, stepC))))
 
-        step5.isCompleted() shouldBe false
-        step1.completed = true
-        step5.isCompleted() shouldBe true
+        step.isCompleted() shouldBe false
+        stepB.completed = true
+        step.isCompleted() shouldBe true
     }
 
-    "Step.And with nested step" {
-        val step1 = getStep()
-        val step2 = getStep()
-        val step3 = Step.Or(listOf(step1, step2))
-        val step4 = getStep()
-        val step5 = Step.And(listOf(step3, step4))
+    "Condition to complete (A AND (B OR C))"  {
+        val stepA = getStep()
+        val stepB = getStep()
+        val stepC = getStep()
+        val step = Step.And(listOf(stepA, Step.Or(listOf(stepB, stepC))))
 
-        step5.isCompleted() shouldBe false
-        step1.completed = true
-        step5.isCompleted() shouldBe false
-        step4.completed = true
-        step5.isCompleted() shouldBe true
+        step.isCompleted() shouldBe false
+        stepA.completed = true
+        step.isCompleted() shouldBe false
+        stepB.completed = true
+        step.isCompleted() shouldBe true
     }
 
     "A OR B resolution" {
