cb483456b82d27bdf4ffb5964303c30d926036ff
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-04-15 16:57:01 +0200

    [webui] Add purgecss to reduce css file size of production builds

--- webui/postcss.config.js
+++ webui/postcss.config.js
@@ -1,6 +1,13 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
-}
+};
