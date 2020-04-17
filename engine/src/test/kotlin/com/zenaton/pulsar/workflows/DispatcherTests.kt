567780bc60aa1ec5c88f152ef28d9c4352fe6e16
Gilles Barbier <gilles@zenaton.com>
2020-04-18 01:09:42 +0200

    move test directory to src

---
+++ engine/src/test/kotlin/com/zenaton/pulsar/workflows/DispatcherTests.kt
@@ -0,0 +1,44 @@
+package com.zenaton.pulsar.workflows
+
+import com.zenaton.engine.workflows.WorkflowState
+import com.zenaton.pulsar.serializer.MessageSerDeInterface
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.string
+import io.mockk.every
+import io.mockk.mockk
+import org.apache.pulsar.functions.api.Context
+import java.nio.ByteBuffer
+
+class DispatcherTests : StringSpec({
+    "Dispatcher.getState with no state should return null" {
+        // mocking
+        val context = mockk<Context>()
+        // given
+        val key = Arb.string(1).toString()
+        every { context.getState(key) } returns null
+        val dispatcher = Dispatcher(context)
+        // when
+        val state = dispatcher.getState(key)
+        // then
+        state shouldBe null
+    }
+
+    "Dispatcher.getState state should return deserialized state" {
+        // mocking
+        val context = mockk<Context>()
+        val serde = mockk<MessageSerDeInterface>()
+        val mockByteBuffer = mockk<ByteBuffer>()
+        val mockWorkflowState = mockk<WorkflowState>()
+        // given
+        val key = Arb.string(1).toString()
+        every { context.getState(key) } returns mockByteBuffer
+        every { serde.deSerializeState(mockByteBuffer) } returns mockWorkflowState
+        val dispatcher = Dispatcher(context, serde)
+        // when
+        val state = dispatcher.getState(key)
+        // then
+        state shouldBe mockWorkflowState
+    }
+})
