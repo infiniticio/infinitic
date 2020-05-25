24c2f8452ba70c18926f0e3f0c70c03a0e63f29c
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-05-25 10:44:37 +0200

    [engine] Move counter manipulations in dedicated private functions

--- engine/src/main/kotlin/com/zenaton/taskmanager/metrics/TaskMetrics.kt
+++ engine/src/main/kotlin/com/zenaton/taskmanager/metrics/TaskMetrics.kt
@@ -5,16 +5,24 @@ import com.zenaton.taskmanager.messages.metrics.TaskStatusUpdated
 import com.zenaton.taskmanager.state.StateStorage
 
 class TaskMetrics(private val stateStorage: StateStorage) {
-    fun handle(msg: TaskMetricMessage) {
-        if (msg is TaskStatusUpdated) {
-            msg.oldStatus?.let {
-                val counterKey = "metrics.rt.counter.task.${msg.taskName.name.toLowerCase()}.${it.toString().toLowerCase()}"
-                stateStorage.incrCounter(counterKey, -1)
-            }
-            msg.newStatus?.let {
-                val counterKey = "metrics.rt.counter.task.${msg.taskName.name.toLowerCase()}.${it.toString().toLowerCase()}"
-                stateStorage.incrCounter(counterKey, 1)
-            }
+    fun handle(message: TaskMetricMessage) {
+        if (message is TaskStatusUpdated) {
+            decrementOldCounter(message)
+            incrementNewCounter(message)
+        }
+    }
+
+    private fun decrementOldCounter(message: TaskStatusUpdated) {
+        message.oldStatus?.let {
+            val counterKey = "metrics.rt.counter.task.${message.taskName.name.toLowerCase()}.${it.toString().toLowerCase()}"
+            stateStorage.incrCounter(counterKey, -1)
+        }
+    }
+
+    private fun incrementNewCounter(message: TaskStatusUpdated) {
+        message.newStatus?.let {
+            val counterKey = "metrics.rt.counter.task.${message.taskName.name.toLowerCase()}.${it.toString().toLowerCase()}"
+            stateStorage.incrCounter(counterKey, 1)
         }
     }
 }
