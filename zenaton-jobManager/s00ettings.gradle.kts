e8be6f671d45e13ec03d76085e2946e830b72fb4
Gilles Barbier <gilles@leetix.com>
2020-06-09 09:02:58 +0200

    cleanup gitignore

--- zenaton-jobManager/s00ettings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "jobManager"
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-        maven(url = "https://dl.bintray.com/gradle/gradle-plugins")
-    }
-}
