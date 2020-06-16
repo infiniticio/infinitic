500442799cba4d8c727f95dde9da2330981ebae1
Gilles Barbier <gilles@leetix.com>
2020-06-16 10:53:04 +0200

    remove node_modules and dist from zenaton-examples

--- node/packages/zenaton-examples/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./examples.cjs.production.min.js')
-} else {
-  module.exports = require('./examples.cjs.development.js')
-}
