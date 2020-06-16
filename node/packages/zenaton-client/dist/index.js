5a39ac0c643852e151d22b7740e0ffc7af216cad
Gilles Barbier <gilles@leetix.com>
2020-06-16 10:51:51 +0200

    remove node_modules and dist from zenaton-client

--- node/packages/zenaton-client/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./client.cjs.production.min.js')
-} else {
-  module.exports = require('./client.cjs.development.js')
-}
--- node/packages/zenaton-examples/dist/index.js
+++ node/packages/zenaton-examples/dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./zenaton-examples.cjs.production.min.js')
+  module.exports = require('./examples.cjs.production.min.js')
 } else {
-  module.exports = require('./zenaton-examples.cjs.development.js')
+  module.exports = require('./examples.cjs.development.js')
 }
