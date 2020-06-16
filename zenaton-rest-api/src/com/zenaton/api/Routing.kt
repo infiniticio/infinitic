f9f225472b8fb3f8f7aa64bed41a7c205e45af1e
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-06-16 10:10:36 +0200

    [api] Add task type metrics endpoint

--- zenaton-rest-api/src/com/zenaton/api/Routing.kt
+++ zenaton-rest-api/src/com/zenaton/api/Routing.kt
@@ -4,11 +4,14 @@ import com.zenaton.api.extensions.io.ktor.application.*
 import com.zenaton.api.task.repositories.TaskRepository
 import com.zenaton.commons.utils.avro.AvroSerDe
 import com.zenaton.jobManager.states.AvroMonitoringGlobalState
+import com.zenaton.jobManager.states.AvroMonitoringPerNameState
 import io.ktor.application.*
+import io.ktor.features.NotFoundException
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.apache.pulsar.client.admin.PulsarAdmin
+import org.apache.pulsar.client.admin.PulsarAdminException
 import org.koin.ktor.ext.inject
 import java.nio.ByteBuffer
 
@@ -18,12 +21,29 @@ fun Routing.root() {
 
     get("/task-types/") {
         val state =
-            pulsarAdmin.functions().getFunctionState("public", "default", "MonitoringGlobalPulsarFunction", "monitoringGlobal.state")?.let { AvroSerDe.deserialize<AvroMonitoringGlobalState>(ByteBuffer.wrap(it.stringValue.toByteArray())) }
-                    ?: return@get
+            pulsarAdmin.functions().getFunctionState("public", "default", "tasks-MonitoringGlobalPulsarFunction", "monitoringGlobal.state")?.let { AvroSerDe.deserialize<AvroMonitoringGlobalState>(ByteBuffer.wrap(it.stringValue.toByteArray())) } ?: return@get
 
         val jobs = state.jobNames.map { object { val name = it } }
 
         call.respond(jobs)
+    }
+
+    get("/task-types/{name}/metrics") {
+        val name = call.getPath<String>("name")
+        try {
+            val state = pulsarAdmin.functions().getFunctionState("public", "default", "tasks-MonitoringPerNamePulsarFunction", "monitoringPerName.state.${name}").let { AvroSerDe.deserialize<AvroMonitoringPerNameState>(ByteBuffer.wrap(it.stringValue.toByteArray())) }
+
+            call.respond(object {
+                val name = name
+                val runningOkCount = state.runningOkCount
+                val runningWarningCount = state.runningWarningCount
+                val runningErrorCount = state.runningErrorCount
+                val terminatedCompletedCount = state.terminatedCompletedCount
+                val terminatedCanceledCount = state.terminatedCanceledCount
+            })
+        } catch (exception: PulsarAdminException.NotFoundException) {
+            throw NotFoundException()
+        }
     }
 
     get("/tasks/{id}") {
