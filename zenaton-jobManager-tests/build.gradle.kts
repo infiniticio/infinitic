bda795288a59a0faecedbf7997162471d3d209ff
Gilles Barbier <gilles@zenaton.com>
2020-08-05 00:40:28 +0200

    add zenaton-jobManager-tests repo

---
+++ zenaton-jobManager-tests/build.gradle.kts
@@ -0,0 +1,44 @@
+
+plugins {
+    kotlin("jvm")
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.apache.avro:avro:1.9.+")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.+")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.+")
+    implementation("org.slf4j:slf4j-api:1.7.+")
+
+    testImplementation(project(":zenaton-avro"))
+    testImplementation(project(":zenaton-jobManager-common"))
+    testImplementation(project(":zenaton-jobManager-engine"))
+    testImplementation(project(":zenaton-jobManager-client"))
+    testImplementation(project(":zenaton-jobManager-worker"))
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    testImplementation("org.jeasy:easy-random-core:4.2.+")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.1.+")
+    testImplementation("io.kotest:kotest-property-jvm:4.1.+")
+    testImplementation("io.kotest:kotest-core-jvm:4.1.+")
+    testImplementation("io.mockk:mockk:1.9.+")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
