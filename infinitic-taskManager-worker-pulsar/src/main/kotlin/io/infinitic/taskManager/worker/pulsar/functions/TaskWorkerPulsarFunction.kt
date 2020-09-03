9d92dd7adabe80eb076138c777c1015d14e5ed90
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-09-03 11:56:37 +0200

    Add first implementation of pulsar specific worker and client

---
+++ infinitic-taskManager-worker-pulsar/src/main/kotlin/io/infinitic/taskManager/worker/pulsar/functions/TaskWorkerPulsarFunction.kt
@@ -0,0 +1,27 @@
+package io.infinitic.taskManager.worker.pulsar.functions
+
+import io.infinitic.taskManager.messages.envelopes.AvroEnvelopeForWorker
+import io.infinitic.taskManager.worker.Dispatcher
+import io.infinitic.taskManager.worker.Worker
+import io.infinitic.taskManager.worker.pulsar.dispatcher.PulsarAvroDispatcher
+import kotlinx.coroutines.runBlocking
+import org.apache.pulsar.functions.api.Context
+import org.apache.pulsar.functions.api.Function
+
+open class TaskWorkerPulsarFunction : Function<AvroEnvelopeForWorker, Void> {
+    var worker = Worker()
+
+    override fun process(input: AvroEnvelopeForWorker, context: Context?): Void? = runBlocking {
+        val ctx = context ?: throw NullPointerException("Null Context received")
+
+        try {
+            worker.dispatcher = Dispatcher(PulsarAvroDispatcher(ctx))
+            worker.handle(input)
+        } catch (e: Exception) {
+            ctx.logger.error("Error:%s for message:%s", e, input)
+            throw e
+        }
+
+        return@runBlocking null
+    }
+}
