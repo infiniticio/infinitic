17898e395ddeca79f553f72360f0a544ad345a9f
Gilles Barbier <gilles@zenaton.com>
2020-09-25 15:57:37 +0200

    refactor task worker test as worker can not instantiate task itself

--- infinitic-taskManager-worker/src/test/kotlin/io/infinitic/taskManager/worker/WorkerTests.kt
+++ infinitic-taskManager-worker/src/test/kotlin/io/infinitic/taskManager/worker/WorkerTests.kt
@@ -13,7 +13,6 @@ import io.infinitic.taskManager.common.data.TaskName
 import io.infinitic.taskManager.common.data.TaskOptions
 import io.infinitic.taskManager.common.data.TaskOutput
 import io.infinitic.taskManager.common.exceptions.ClassNotFoundDuringInstantiation
-import io.infinitic.taskManager.common.exceptions.ErrorDuringInstantiation
 import io.infinitic.taskManager.common.exceptions.InvalidUseOfDividerInTaskName
 import io.infinitic.taskManager.common.exceptions.MultipleUseOfDividerInTaskName
 import io.infinitic.taskManager.common.exceptions.NoMethodFoundWithParameterCount
@@ -26,6 +25,13 @@ import io.infinitic.taskManager.common.messages.RunTask
 import io.infinitic.taskManager.common.messages.TaskAttemptCompleted
 import io.infinitic.taskManager.common.messages.TaskAttemptFailed
 import io.infinitic.taskManager.common.messages.TaskAttemptStarted
+import io.infinitic.taskManager.worker.samples.SampleTask
+import io.infinitic.taskManager.worker.samples.TestingSampleTask
+import io.infinitic.taskManager.worker.samples.TestingSampleTaskWithRetry
+import io.infinitic.taskManager.worker.samples.TestingSampleTaskWithBadTypeRetry
+import io.infinitic.taskManager.worker.samples.TestingSampleTaskWithBuggyRetry
+import io.infinitic.taskManager.worker.samples.TestingSampleTaskWithContext
+import io.infinitic.taskManager.worker.samples.TestingSampleTaskWithTimeout
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
@@ -33,7 +39,7 @@ import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.just
 import io.mockk.mockk
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.coroutineScope
 
 class WorkerTests : StringSpec({
     val dispatcher = mockk<Dispatcher>()
@@ -50,9 +56,10 @@ class WorkerTests : StringSpec({
         val input = arrayOf(2, "3")
         val types = listOf(Int::class.java.name, String::class.java.name)
         // with
-        val msg = getRunTask(TestWithoutRetry::class.java.name, input, types)
+        val msg = getRunTask(SampleTask::class.java.name, input, types)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register<SampleTask>(TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
 
@@ -71,9 +78,10 @@ class WorkerTests : StringSpec({
         val input = arrayOf(3, "3")
         val types = listOf(Int::class.java.name, String::class.java.name)
         // with
-        val msg = getRunTask("${TestWithoutRetryAndExplicitMethod::class.java.name}::run", input, types)
+        val msg = getRunTask("foo::explicitMethodWithoutRetry", input, types)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
 
@@ -91,9 +99,10 @@ class WorkerTests : StringSpec({
     "Should be able to run an explicit method with 2 parameters without parameterTypes" {
         val input = arrayOf(4, "3")
         // with
-        val msg = getRunTask("${TestWithoutRetryAndExplicitMethod::class.java.name}::run", input, null)
+        val msg = getRunTask("foo::explicitMethodWithoutRetry", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
 
@@ -110,39 +119,18 @@ class WorkerTests : StringSpec({
 
     "Should fail when trying to register an invalid task name " {
         shouldThrow<InvalidUseOfDividerInTaskName> {
-            Worker.register("blabla::", TestWithoutRetry())
+            Worker.register("foo::", TestingSampleTask())
         }
     }
 
-    "Should be able to use a registered task name " {
-        val input = arrayOf(2, "3")
-        val types = listOf(Int::class.java.name, String::class.java.name)
-        // with
-        val msg = getRunTask("blabla", input, types)
-        // when
-        Worker.register("blabla", TestWithoutRetry())
-        launch { worker.runTask(msg) }.join()
-        // then
-        slots.size shouldBe 2
-
-        slots[0] shouldBe getTaskAttemptStarted(msg)
-
-        slots[1] shouldBe TaskAttemptCompleted(
-            taskId = msg.taskId,
-            taskAttemptId = msg.taskAttemptId,
-            taskAttemptIndex = msg.taskAttemptIndex,
-            taskAttemptRetry = msg.taskAttemptRetry,
-            taskOutput = TaskOutput("6")
-        )
-    }
-
-    "Should throw _root_ide_package_.io.infinitic.taskManager.common.exceptions.MultipleUseOfDividerInTaskNameme when trying to process an invalid task name " {
+    "Should throw MultipleUseOfDividerInTaskName when trying to process an invalid task name " {
         val input = arrayOf(2, "3")
         val types = listOf(Int::class.java.name, String::class.java.name)
         // with
         val msg = getRunTask("blabla::m1::m2", input, types)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("blabla", TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -160,10 +148,10 @@ class WorkerTests : StringSpec({
         val input = arrayOf(2, "3")
         val types = listOf(Int::class.java.name, String::class.java.name)
         // with
-        val msg = getRunTask("blabla", input, types)
+        val msg = getRunTask("foo", input, types)
         // when
-        Worker.unregister("blabla")
-        launch { worker.runTask(msg) }.join()
+        Worker.unregister("foo")
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
 
@@ -179,33 +167,14 @@ class WorkerTests : StringSpec({
         fail.taskAttemptError.data!!::class.java.name shouldBe ClassNotFoundDuringInstantiation::class.java.name
     }
 
-    "Should throw ErrorDuringTaskInstantiation when if impossible to create new instance" {
+    "Should throw NoMethodFoundWithParameterTypes when trying to process an unknown method" {
         val input = arrayOf(2, "3")
         val types = listOf(Int::class.java.name, String::class.java.name)
         // with
-        val msg = getRunTask(TestWithConstructor::class.java.name, input, types)
+        val msg = getRunTask("foo::unknown", input, types)
         // when
-        launch { worker.runTask(msg) }.join()
-        // then
-        slots.size shouldBe 2
-        slots[0] shouldBe getTaskAttemptStarted(msg)
-        (slots[1] is TaskAttemptFailed) shouldBe true
-        val fail = slots[1] as TaskAttemptFailed
-        fail.taskId shouldBe msg.taskId
-        fail.taskAttemptId shouldBe msg.taskAttemptId
-        fail.taskAttemptIndex shouldBe msg.taskAttemptIndex
-        fail.taskAttemptRetry shouldBe msg.taskAttemptRetry
-        fail.taskAttemptDelayBeforeRetry shouldBe null
-        fail.taskAttemptError.data!!::class.java.name shouldBe ErrorDuringInstantiation::class.java.name
-    }
-
-    "Should throw NoMethodFoundWithParameterTypes  when trying to process an unknown method" {
-        val input = arrayOf(2, "3")
-        val types = listOf(Int::class.java.name, String::class.java.name)
-        // with
-        val msg = getRunTask("${TestWithoutRetry::class.java.name}::unknown", input, types)
-        // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -222,9 +191,10 @@ class WorkerTests : StringSpec({
     "Should throw NoMethodFoundWithParameterCount when trying to process an unknown method without parameterTypes" {
         val input = arrayOf(2, "3")
         // with
-        val msg = getRunTask("${TestWithoutRetry::class.java.name}::unknown", input, null)
+        val msg = getRunTask("foo::unknown", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -241,9 +211,10 @@ class WorkerTests : StringSpec({
     "Should throw TooManyMethodsFoundWithParameterCount when trying to process an unknown method without parameterTypes" {
         val input = arrayOf(2, "3")
         // with
-        val msg = getRunTask("${TestWithoutRetry::class.java.name}::handle", input, null)
+        val msg = getRunTask("foo::handle", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTask())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -260,9 +231,10 @@ class WorkerTests : StringSpec({
     "Should retry with correct exception" {
         val input = arrayOf(2, "3")
         // with
-        val msg = getRunTask(TestWithRetry::class.java.name, input, null)
+        val msg = getRunTask("foo", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTaskWithRetry())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -279,9 +251,10 @@ class WorkerTests : StringSpec({
     "Should throw RetryDelayReturnTypeError when getRetryDelay has wrong return type" {
         val input = arrayOf(2, "3")
         // with
-        val msg = getRunTask(TestWithBadRetryType::class.java.name, input, null)
+        val msg = getRunTask("foo", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTaskWithBadTypeRetry())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -298,9 +271,10 @@ class WorkerTests : StringSpec({
     "Should throw when getRetryDelay throw an exception" {
         val input = arrayOf(2, "3")
         // with
-        val msg = getRunTask(TestWithBuggyRetry::class.java.name, input, null)
+        val msg = getRunTask("foo", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTaskWithBuggyRetry())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -317,9 +291,10 @@ class WorkerTests : StringSpec({
     "Should be able to access context from task" {
         val input = arrayOf(2, "3")
         // with
-        val msg = getRunTask(TestWithContext::class.java.name, input, null)
+        val msg = getRunTask("foo", input, null)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTaskWithContext())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -336,9 +311,10 @@ class WorkerTests : StringSpec({
         val input = arrayOf(2, "3")
         val types = listOf(Int::class.java.name, String::class.java.name)
         // with
-        val msg = getRunTask(TestWithTimeout::class.java.name, input, types)
+        val msg = getRunTask("foo", input, types)
         // when
-        launch { worker.runTask(msg) }.join()
+        Worker.register("foo", TestingSampleTaskWithTimeout())
+        coroutineScope { worker.runTask(msg) }
         // then
         slots.size shouldBe 2
         slots[0] shouldBe getTaskAttemptStarted(msg)
@@ -352,54 +328,6 @@ class WorkerTests : StringSpec({
         fail.taskAttemptError.data!!::class.java.name shouldBe ProcessingTimeout::class.java.name
     }
 })
-
-internal class TestWithoutRetry {
-    fun handle(i: Int, j: String) = (i * j.toInt()).toString()
-    fun handle(i: Int, j: Int) = (i * j).toString()
-}
-
-internal class TestWithoutRetryAndExplicitMethod {
-    fun run(i: Int, j: String) = (i * j.toInt()).toString()
-}
-
-internal class TestWithRetry {
-    lateinit var context: TaskAttemptContext
-
-    fun handle(i: Int, j: String): String = if (i < 0) (i * j.toInt()).toString() else throw IllegalStateException()
-    fun getRetryDelay(): Float? = if (context.exception is IllegalStateException) 3F else 0F
-}
-
-internal class TestWithBuggyRetry {
-    lateinit var context: TaskAttemptContext
-
-    fun handle(i: Int, j: String): String = if (i < 0) (i * j.toInt()).toString() else throw IllegalStateException()
-    fun getRetryDelay(): Float? = if (context.exception is IllegalStateException) throw IllegalArgumentException() else 3F
-}
-
-internal class TestWithBadRetryType {
-    fun handle(i: Int, j: String): String = if (i < 0) (i * j.toInt()).toString() else throw IllegalStateException()
-    fun getRetryDelay() = 3
-}
-
-internal class TestWithConstructor(val value: String) {
-    fun handle(i: Int, j: String) = (i * j.toInt()).toString()
-}
-
-internal class TestWithContext() {
-    private lateinit var context: TaskAttemptContext
-
-    fun handle(i: Int, j: String) = (i * j.toInt() * context.taskAttemptIndex.int).toString()
-}
-
-internal class TestWithTimeout() {
-    lateinit var context: TaskAttemptContext
-
-    fun handle(i: Int, j: String): String {
-        Thread.sleep(400)
-
-        return (i * j.toInt() * context.taskAttemptIndex.int).toString()
-    }
-}
 
 private fun getRunTask(name: String, input: Array<out Any?>, types: List<String>?) = RunTask(
     taskId = TaskId(),
---
+++ infinitic-taskManager-worker/src/test/kotlin/io/infinitic/taskManager/worker/samples/SampleTaskWithRetry.kt
@@ -0,0 +1,27 @@
+package io.infinitic.taskManager.worker.samples
+
+import io.infinitic.taskManager.worker.TaskAttemptContext
+
+internal class TestingSampleTaskWithRetry() {
+    lateinit var context: TaskAttemptContext
+
+    fun handle(i: Int, j: String): String = if (i < 0) (i * j.toInt()).toString() else throw IllegalStateException()
+
+    fun getRetryDelay(): Float? = if (context.exception is IllegalStateException) 3F else 0F
+}
+
+internal class TestingSampleTaskWithBadTypeRetry() {
+    lateinit var context: TaskAttemptContext
+
+    fun handle(i: Int, j: String): String = if (i < 0) (i * j.toInt()).toString() else throw IllegalStateException()
+
+    fun getRetryDelay(): Int? = 3
+}
+
+internal class TestingSampleTaskWithBuggyRetry() {
+    lateinit var context: TaskAttemptContext
+
+    fun handle(i: Int, j: String): String = if (i < 0) (i * j.toInt()).toString() else throw IllegalStateException()
+
+    fun getRetryDelay(): Float? = if (context.exception is IllegalStateException) throw IllegalArgumentException() else 3F
+}
