6d1df0f303e3223725839a1cd0cfa43f1c28069b
Gilles Barbier <gilles@zenaton.com>
2021-05-19 16:14:26 +0200

    0.6.4 version

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -27,7 +27,7 @@ object Ci {
 
     // this is the version used for building snapshots
     // .GITHUB_RUN_NUMBER-snapshot will be appended
-    private const val snapshotBase = "0.6.3"
+    private const val snapshotBase = "0.6.4"
 
     private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
 
