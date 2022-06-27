90e274c935fb41668830826fc24a70359d8a184f
Gilles <gilles@infinitic.io>
2022-06-28 01:43:03 +0200

    fix BytesSchemaVersion collision in KSchemaReader

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -27,7 +27,7 @@ object Ci {
 
     // this is the version used for building snapshots
     // .GITHUB_RUN_NUMBER-snapshot will be appended
-    const val base = "0.9.10"
+    const val base = "0.9.11"
 
     private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
 
