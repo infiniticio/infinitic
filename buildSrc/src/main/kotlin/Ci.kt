5141a1adab578176fd803442e3501324611d650c
Gilles <gilles@infinitic.io>
2023-06-16 19:44:08 +0200

    version 0.11.6

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -24,7 +24,7 @@ object Ci {
 
   // this is the version used for building snapshots
   // .GITHUB_RUN_NUMBER-snapshot will be appended
-  const val base = "0.11.5"
+  const val base = "0.11.6"
 
   private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
 
