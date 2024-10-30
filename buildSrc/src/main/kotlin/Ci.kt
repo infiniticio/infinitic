8f459b9bead58e49d2d5f8fbc40ba7d76e85e370
Gilles Barbier <gilles@infinitic.io>
2024-10-30 14:31:46 +0100

    Bump version to 0.16.2

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -25,7 +25,7 @@ object Ci {
   private const val SNAPSHOT = "-SNAPSHOT"
 
   // base version number
-  private const val BASE = "0.16.1"
+  private const val BASE = "0.16.2"
 
   // GitHub run number
   private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
