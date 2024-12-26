4a91372f8e72234950311e8acbd54688e642ec4f
Gilles Barbier <gilles@infinitic.io>
2024-12-26 15:40:07 +0100

    bug fix: workaround to https://youtrack.jetbrains.com/issue/KT-71057

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -25,7 +25,7 @@ object Ci {
   private const val SNAPSHOT = "-SNAPSHOT"
 
   // base version number
-  private const val BASE = "0.16.3"
+  private const val BASE = "0.16.4"
 
   // GitHub run number
   private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
